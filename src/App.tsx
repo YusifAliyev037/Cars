42e02ddff3124bd84cf6bd66ee9881ed19f58324
YusifAliyev037 <131958118+YusifAliyev037@users.noreply.github.com>
2024-08-01 22:20:45 +0400

    section1finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,62 @@
 import './App.css'
-import "@mantine/core/styles.css"
+// import "@mantine/core/styles.css"
 import Mainmodal from './components/MainModal/Mainmodal'
+
+
 
 
 function App() {
 
+  const currentDate = new Date().toDateString()
+  console.log(currentDate);
+
+  
+
   return (
     <>
+    <div className='content'>
       <Mainmodal/>
+      <div className='section2'>
+         <div className='maintitle'>
+          <h2>Bugünün Statistikası</h2>
+          <p>{currentDate}</p>
+         </div>
+         <div className='card'>
+          <div className='title'>
+            <h4>Gəlir</h4>
+            <p className='subtitle'>Bugün</p>
+          </div>
+          <hr style={{color:"gray"}} />
+          <div className='price'>
+            <h1>$9400</h1>
+            <p >1.5%</p>   {/* color red and green */}
+          </div>
+          <div>
+            <p>Dünən olan gəlir:123$</p>
+            <h4>Keçən həftənin gəliri:1232$</h4>
+          </div>
+         
+         </div>
+         <div className='card'>
+          <div className='title'>
+            <h4>Xərc</h4>
+            <p className='subtitle'>Bugün</p>
+          </div>
+          <hr style={{color:"gray"}} />
+          <div className='price'>
+            <h1>$9400</h1>
+            <p >1.5%</p>   {/* color red and green */}
+          </div>
+          <div>
+            <p>Dünən olan xərc:123$</p>
+            <h4>Keçən həftənin xərc:1232$</h4>
+          </div>
+         
+         </div>
+
+      </div>
+      <div className='section3'>salam</div>
+    </div>
     </>
   )
 }
